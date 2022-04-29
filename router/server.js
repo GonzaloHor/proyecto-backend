acfe254ef6a0fb2fb7d6acca1bf3ba319a405686
cls <gonzalo_horton@outlook.com>
2022-04-28 23:31:52 -0300

    Segunda

--- router/server.js
+++ router/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { Router } = express
 const app = express()
-const router = Router()
 const fs = require('fs')
-const productosController = require('../controllers/productosController');
-const carritoController = require('../controllers/carritoController')
+const router = require('../controllers/Controller.js')
 
+
+
+
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
 
 
 app.use(express.json())
@@ -13,32 +19,174 @@ app.use(express.urlencoded({extended: true}))
 
 
 
-app.use('/', express.static(__dirname + '/public/index.html'));
+class creteFile{
+    constructor(nameFile){
+        this.nameFile = nameFile;   
+    };
+    
+    readFile(){
+        const ruta = `./${this.nameFile}.txt`
+
+        try {
+            const datas = fs.readFileSync(ruta,'utf-8');
+            const dataJson = JSON.parse(datas);
 
 
-app.listen(() => {
-    try {
-        console.log(`Servidor funcionando`)
+             return dataJson
+ 
+        } catch (error){
+            if(error.message == "Unexpected end of JSON input"){
+                let mensaje = "No hay contenido"
+                return mensaje
+            }
+               
+            console.log(`Hubo un error: ${error.message}`);
+            return
+        }
 
-    } catch (error){
-        console.log(`Hubo un error: ${error}`);
     }
+
+    writeFile(producto){
+        const ruta = `./${this.nameFile}.txt`
+
+        try {
+
+        
+            fs.writeFileSync(ruta,JSON.stringify(producto, null, '\t'));
+       
+ 
+        } catch (error){
+            console.log(`Hubo un error: ${error}`);
+        }
+
+    }
+    
+
+}
+
+let productos = new creteFile('Productos');
+let historialMensajes = new creteFile('historialMensajes');
+
+
+
+httpServer.listen(8080, function() {
+    console.log('Servidor corriendo en http://localhost:8080');
 })
 
 
 
-app.use('/api/productos', productosController)
-app.use('/api/carrito', carritoController)
+let todosLosProductos = productos.readFile();
+let mensajes = historialMensajes.readFile();
 
-app.use('*', (req,res) => res.send({ 
-    "error" : -2, 
-    "ruta": req.baseUrl,
-    "método": req.method,
-    "descripcion": 'no implementada'
-    }
-    ))
+// Conexión con Socket.io 
+io.on('connection',socket => {
+    socket.emit('productos',todosLosProductos);
+    socket.emit('mensajes', mensajes);
+
+    socket.on('new-producto', (data) => {
+
+        let todosLosProductos = productos.readFile();
+
+        if(data.Title == "" || data.Price == "" || data.Thumbnail == ""){
+        alert("Completa todos los campos")
+        return
+        }
+
+        data.id = todosLosProductos.length + 1
+    
+        todosLosProductos.push(data)
+    
+        productos.writeFile(todosLosProductos)
+
+        todosLosProductos = productos.readFile();
+
+
+        io.sockets.emit('productos', todosLosProductos);
+    });
+
+    socket.on('new-mensaje',(nuevoMensaje) => {
+
+        let mensajes = historialMensajes.readFile();
+        
+                
+                if(mensajes == "No hay contenido"){nuevoMensaje.id = 1
+                } 
+                else { nuevoMensaje.id = mensajes.length + 1 }
+               
+                
+                // Obtener fecha y hora del msg
+                let dataTime = new Date();
+                let hoy = new Date(dataTime)
+            
+                let fecha = hoy.toLocaleDateString()
+                let hora = hoy.toLocaleTimeString()
+        
+                let tiempoMensaje =  `${fecha} ${hora} `
+        
+                nuevoMensaje.dataTimeMensaje = tiempoMensaje
+        
+                if(mensajes == "No hay contenido"){
+        
+                    nuevoMensaje = [nuevoMensaje]
+                    
+                    historialMensajes.writeFile(nuevoMensaje)
+        
+                } else{
+                    mensajes.push(nuevoMensaje)
+            
+                    historialMensajes.writeFile(mensajes)
+                }
+             
+              
+        
+                mensajes = historialMensajes.readFile();
+        
+                
+        
+                io.sockets.emit('mensajes', mensajes);
+
+    });
+
+
+    socket.on('delete-product', (id) =>{
+        let todosLosProductos = productos.readFile();
+
+        todosLosProductos = todosLosProductos.filter((item) => item.id !== id);
+
+        productos.writeFile(todosLosProductos)
+        
+        io.sockets.emit('productos', todosLosProductos);
+
+    })
+
+
+});
 
 
 
-app.listen(8080)
- 
+
+
+app.use(express.static("public"));
+app.use('/api', router)
+
+
+app.route('/',(req, res) => {
+
+    console.log("Pagina regargada")
+
+    let todosLosProductos = productos.readFile();
+    let mensajes = historialMensajes.readFile();
+
+    io.on('connection',socket => {
+        socket.emit('productos',todosLosProductos);
+        socket.emit('mensajes', mensajes);
+    
+    })
+   
+       res.redirect('/')
+   
+   
+   })
+
+
+
